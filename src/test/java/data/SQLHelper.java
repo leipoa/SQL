bca91d435a9d05a9b6b6096716a19cac79afca1b
Darya Vylomova <qwerty6108@gmail.com>
2023-12-11 22:17:02 +0300

    second commit

--- src/test/java/data/SQLHelper.java
+++ src/test/java/data/SQLHelper.java
@@ -1,8 +1,11 @@
 package data;
+
 import lombok.SneakyThrows;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
-import org.openqa.selenium.bidi.Connection;
+
+import java.sql.Connection;
+
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,31 +13,33 @@ import java.sql.SQLException;
 public class SQLHelper {
     private static final QueryRunner runner = new QueryRunner();
 
-    private SQLHelper(){
+    private SQLHelper() {
     }
 
-    private static Connection getConn() throws SQLException{
-        return (Connection) DriverManager.getConnection("jdbs:mysql://localhost:3306/app","app", "pass");
+    private static Connection getConn() throws SQLException {
+        return (Connection) DriverManager.getConnection("jdbs:mysql://localhost:3306/app", "app", "pass");
     }
 
     @SneakyThrows
-    public static DataHelper.VerificationCode getVerificationCode(){
+    public static DataHelper.VerificationCode getVerificationCode() {
         var codeSQL = "SELECT code FROM auth_codes ORDER BY created DESC LIMIT 1";
         var conn = getConn();
-        var code = runner.query((java.sql.Connection) conn, codeSQL, new  ScalarHandler<String>());
+        var code = runner.query(conn, codeSQL, new ScalarHandler<String>());
         return new DataHelper.VerificationCode(code);
     }
+
     @SneakyThrows
     public static void cleanDatabase() {
         var connection = getConn();
-        runner.execute((java.sql.Connection) connection, "DELETE FROM auth_codes");
-        runner.execute((java.sql.Connection) connection, "DELETE FROM cards_transactions");
-        runner.execute((java.sql.Connection) connection, "DELETE FROM users");
-        runner.execute((java.sql.Connection) connection, "DELETE FROM cards");
+        runner.execute(connection, "DELETE FROM auth_codes");
+        runner.execute(connection, "DELETE FROM card_transactions");
+        runner.execute(connection, "DELETE FROM cards");
+        runner.execute(connection, "DELETE FROM users");
     }
+
     @SneakyThrows
-    public static void cleanAuthCodes(){
+    public static void cleanAuthCodes() {
         var connection = getConn();
-        runner.execute((java.sql.Connection) connection, "DELETE FROM auth_codes");
+        runner.execute(connection, "DELETE FROM auth_codes");
     }
 }
--- src/test/java/test/BankLoginTest.java
+++ src/test/java/test/BankLoginTest.java
@@ -2,6 +2,8 @@ package test;
 
 import data.DataHelper;
 import data.SQLHelper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import page.LoginPage;
@@ -12,35 +14,46 @@ import static data.SQLHelper.cleanDatabase;
 
 public class BankLoginTest {
     LoginPage loginPage;
-    void tearDown() {cleanAuthCodes();
+
+    @AfterEach
+    void tearDown() {
+        cleanAuthCodes();
     }
-    static void tearDownAll(){cleanDatabase();
+
+    @AfterAll
+    static void tearDownAll() {
+        cleanDatabase();
     }
+
     @BeforeEach
-    void setUp(){ open("http://localhost:9999", LoginPage.class);
+    void setUp() {
+        loginPage = open("http://localhost:9999/", LoginPage.class);
     }
+
     @Test
-    void shouldSuccessfullyLogin(){
+    void shouldSuccessfullyLogin() {
         var authInfo = DataHelper.getAuthInfoWithTestData();
         var verificationPage = loginPage.validLogin(authInfo);
         verificationPage.verificationPageVisibility();
         var verificationCode = SQLHelper.getVerificationCode();
         verificationPage.validVerify(verificationCode.getCode());
     }
+
     @Test
-    void shouldGetErrorNotificationIfLoginWithRandomUserWithoutAddingToBase(){
+    void shouldGetErrorNotificationIfLoginWithRandomUserWithoutAddingToBase() {
         var authInfo = DataHelper.generateRandomUser();
         loginPage.validLogin(authInfo);
-        loginPage.verifyErrorNotification("Ошибка! ");
+        loginPage.verifyErrorNotification("Ошибка! \nНеверно указан логин или пароль");
     }
+
     @Test
-    void shouldGetErrorNotificationIfLoginWithExistUserAndRandomVerificationCode(){
+    void shouldGetErrorNotificationIfLoginWithExistUserAndRandomVerificationCode() {
         var authInfo = DataHelper.getAuthInfoWithTestData();
         var verificationPage = loginPage.validLogin(authInfo);
         verificationPage.verificationPageVisibility();
         var verificationCode = DataHelper.generateRandomVerificationCode();
         verificationPage.verify(verificationCode.getCode());
-        verificationPage.verifyErrorNotification("Ошибка! ");
+        verificationPage.verifyErrorNotification("Ошибка! \nНеверно указан код! Попробуйте еще раз.");
     }
 
 }
